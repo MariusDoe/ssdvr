e00b8e49d752e5f612ce421fe8553a27aa7ac2e4
Marius Dörbandt <github.marius@doerbandt.de>
2024-05-02 03:06:23 +0200

    Natively render Codemirror editor

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
-import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
+import { javascript } from "@codemirror/lang-javascript";
+import { ViewPlugin, ViewUpdate } from "@codemirror/view";
+import { EditorView, basicSetup } from "codemirror";
 import * as THREE from "three";
 import { VRButton } from "three/addons/webxr/VRButton.js";
 import {
+  FontLoader,
   InteractiveGroup,
   XRControllerModelFactory,
 } from "three/examples/jsm/Addons.js";
-import { HTMLMesh } from "./HTMLCanvas";
-import "./userWorker";
+import consolasBoldItalicUrl from "../fonts/Consolas_Bold Italic.json?url";
+import consolasBoldUrl from "../fonts/Consolas_Bold.json?url";
+import consolasItalicUrl from "../fonts/Consolas_Italic.json?url";
+import consolasUrl from "../fonts/Consolas_Regular.json?url";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -78,12 +83,148 @@ editorContainer.addEventListener("click", (event) => {
   (event.target as HTMLElement | null)?.focus();
 });
 
-const editor = monaco.editor.create(editorContainer, {
-  value: 'function test() {\n\tconsole.log("Hello World!");\n}',
-  language: "typescript",
-  cursorBlinking: "solid",
-});
+const fontLoader = new FontLoader();
+const fonts = await Promise.all(
+  [consolasUrl, consolasBoldUrl, consolasItalicUrl, consolasBoldItalicUrl].map(
+    (url) => fontLoader.loadAsync(url)
+  )
+);
+const fontFromFlags = (bold: boolean, italic: boolean) =>
+  fonts[(+italic << 1) | +bold];
+const fontFromStyle = (style: CSSStyleDeclaration) =>
+  fontFromFlags(parseInt(style.fontWeight) > 400, style.fontStyle === "italic");
 
-const editorMesh = new HTMLMesh(editorContainer);
-editorMesh.position.y = 1;
-interactiveGroup.add(editorMesh);
+const materials: Record<string, THREE.Material> = {};
+const materialFromColor = (color: string) =>
+  (materials[color] ??= new THREE.MeshBasicMaterial({
+    color: color,
+  }));
+const foregroundMaterialFromStyle = (style: CSSStyleDeclaration) =>
+  materialFromColor(style.color);
+const colorAlpha = (color: string) => {
+  const match = color.match(/rgba\([^,]+,[^,],[^,],\s(\S+)\)/);
+  if (match) {
+    return parseFloat(match[1]);
+  } else {
+    return 1;
+  }
+};
+const backgroundMaterialFromStyles = (styles: CSSStyleDeclaration[]) =>
+  materialFromColor(
+    styles
+      .map(
+        (style) =>
+          [
+            new THREE.Color(style.backgroundColor),
+            colorAlpha(style.backgroundColor),
+          ] as const
+      )
+      .reduce(([a, alpha_a], [b, alpha_b]) => [a.lerp(b, alpha_b), alpha_a])[0]
+      .getHexString()
+  );
+
+const viewPlugin = ViewPlugin.fromClass(
+  class {
+    view: EditorView;
+    meshes: THREE.Mesh[] = [];
+
+    constructor(view: EditorView) {
+      this.view = view;
+      this.requestRedraw();
+    }
+
+    requestRedraw() {
+      this.view.requestMeasure({
+        read: () => {
+          this.redraw();
+        },
+      });
+    }
+
+    update(update: ViewUpdate) {
+      if (update.docChanged) {
+        this.requestRedraw();
+      }
+    }
+
+    redraw() {
+      scene.remove(...this.meshes);
+      this.meshes.splice(0);
+      const dom = this.view.contentDOM;
+      const lines = dom.querySelectorAll(".cm-line");
+      const size = 0.1;
+      const { data } = fonts[0];
+      const scale = size / data.resolution;
+      const lineHeight =
+        (data.boundingBox.yMax -
+          data.boundingBox.yMin +
+          data.underlineThickness) *
+        scale;
+      const glyphAdvance = data.glyphs["x"].ha * scale;
+      const width =
+        Math.max(
+          0,
+          ...Array.from(lines).map((line) => (line.textContent ?? "").length)
+        ) * glyphAdvance;
+      const left = -2;
+      const top = 2;
+      let x = left;
+      let y = top;
+      const z = -2;
+      const traverseNode = (node: Node, style: CSSStyleDeclaration) => {
+        if (node instanceof Element) {
+          traverseElement(node);
+          return;
+        }
+        const { textContent } = node;
+        if (textContent === null) {
+          return;
+        }
+        const font = fontFromStyle(style);
+        const shapes = font.generateShapes(textContent, size);
+        for (const shape of shapes) {
+          // fixup shapes so triangulation doesn't break for weird holes arrays
+          if (shape.holes.length === 1 && shape.holes[0].curves.length === 0) {
+            shape.holes = [];
+          }
+        }
+        const geometry = new THREE.ShapeGeometry(shapes);
+        geometry.translate(x, y, z);
+        const material = foregroundMaterialFromStyle(style);
+        const mesh = new THREE.Mesh(geometry, material);
+        this.meshes.push(mesh);
+        x += textContent.length * glyphAdvance;
+      };
+      const parentStyles: CSSStyleDeclaration[] = [];
+      const traverseElement = (element: Element, isLine = false) => {
+        const style = getComputedStyle(element);
+        parentStyles.push(style);
+        if (isLine) {
+          const geometry = new THREE.PlaneGeometry(width, lineHeight);
+          geometry.translate(left + width / 2, y + lineHeight / 4, z - 0.0001);
+          const mesh = new THREE.Mesh(
+            geometry,
+            backgroundMaterialFromStyles(parentStyles)
+          );
+          this.meshes.push(mesh);
+        }
+        for (const child of element.childNodes) {
+          traverseNode(child, style);
+        }
+        parentStyles.pop();
+      };
+      for (const line of lines) {
+        traverseElement(line, true);
+        x = left;
+        y -= lineHeight;
+      }
+      scene.add(...this.meshes);
+    }
+  }
+);
+
+const view = new EditorView({
+  doc: 'function test() {\n  console.log("Hello World!");\n}',
+  parent: document.body,
+  extensions: [basicSetup, javascript({ typescript: true }), viewPlugin],
+});
