e72fde21b9fc3d5612437c6be87419a182268dc9
Marius Dörbandt <github.marius@doerbandt.de>
2024-06-04 18:45:05 +0200

    Refactor controllers

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,16 +1,34 @@
-import { BufferGeometry, Line, LineBasicMaterial, Vector3 } from "three";
+import {
+  BufferGeometry,
+  Line,
+  LineBasicMaterial,
+  Vector3,
+  XRGripSpace,
+  XRTargetRaySpace,
+} from "three";
 import { XRControllerModelFactory } from "three/examples/jsm/Addons.js";
+import { useAdd, useEventListener } from "./dispose-hooks";
+import { onDispose } from "./hmr/dispose";
+import { captureRunningModule } from "./hmr/running-module";
 import { renderer, scene } from "./scene";
+
+export type Controller = {
+  source?: XRInputSource;
+  hand: XRTargetRaySpace;
+  grip: XRGripSpace;
+};
+
+export const controllers: Controller[] = [];
 
 const lineGeometry = new BufferGeometry();
 lineGeometry.setFromPoints([new Vector3(0, 0, 0), new Vector3(0, 0, -5)]);
 
-const controllerIndices = [0, 1];
-const controllers = controllerIndices.map((index) =>
-  renderer.xr.getController(index)
-);
-for (const controller of controllers) {
-  controller.add(
+const controllerModelFactory = new XRControllerModelFactory();
+
+for (let index = 0; index < 2; index++) {
+  const hand = renderer.xr.getController(index);
+  useAdd(
+    hand,
     new Line(
       lineGeometry,
       new LineBasicMaterial({
@@ -18,17 +36,35 @@ for (const controller of controllers) {
       })
     )
   );
-  scene.add(controller);
+  useAdd(scene, hand);
+
+  const grip = renderer.xr.getControllerGrip(index);
+  useAdd(grip, controllerModelFactory.createControllerModel(grip));
+  useAdd(scene, grip);
+
+  const controller = {
+    hand,
+    grip,
+  };
+  controllers.push(controller);
 }
 
-const controllerModelFactory = new XRControllerModelFactory();
+const updateControllers = () => {
+  let index = 0;
+  for (const source of renderer.xr.getSession()!.inputSources) {
+    if (index >= controllers.length) {
+      break;
+    }
+    controllers[index].source = source;
+  }
+};
 
-const controllerGrips = controllerIndices.map((index) =>
-  renderer.xr.getControllerGrip(index)
-);
-for (const controllerGrip of controllerGrips) {
-  controllerGrip.add(
-    controllerModelFactory.createControllerModel(controllerGrip)
-  );
-  scene.add(controllerGrip);
-}
+useEventListener(renderer.xr, "sessionstart", () => {
+  updateControllers();
+  const session = renderer.xr.getSession()!;
+  const listener = captureRunningModule(() => {
+    updateControllers();
+  });
+  session.addEventListener("inputsourceschange", listener);
+  onDispose(() => session.removeEventListener("inputsourceschange", listener));
+});
