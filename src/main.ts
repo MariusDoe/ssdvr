d8cfb23277082f5e20af1cbdee4d724187a4de69
Marius Dörbandt <github.marius@doerbandt.de>
2024-06-07 02:28:54 +0200

    Spawn FilePicker initially

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,22 @@ import "core-js/actual";
 import "./controllers";
 import "./demo";
 import "./editor/editor";
-import { Editor } from "./editor/editor";
-import { read, write } from "./files";
+import { fontFromFlags } from "./editor/fonts";
+import { FilePicker } from "./file-picker";
 import { preserve } from "./hmr/preserve";
+import { materialFromColor } from "./materials";
 import { openInMovable } from "./movable";
 import "./scene";
-import { renderer } from "./scene";
 import "./vr";
 
-preserve("editor", async () => {
-  const editor = new Editor();
-  openInMovable(editor);
-  renderer.xr.addEventListener("sessionstart", () => {
-    editor.focus();
+preserve("file-picker", async () => {
+  const filePicker = new FilePicker(".", {
+    size: 0.1,
+    indent: 0.1,
+    font: fontFromFlags({ bold: false, italic: false }),
+    backgroundMaterial: materialFromColor("darkblue"),
+    foregroundMaterial: materialFromColor("white"),
   });
-  const path = "src/demo.ts";
-  editor.load(await read(path));
-  editor.addEventListener("save", () => {
-    write(path, editor.getDocument());
-  });
+  openInMovable(filePicker);
+  return filePicker;
 });
