40f2e1ec52b88da1457571d87e03e3c3fd8e63ff
Marius Dörbandt <github.marius@doerbandt.de>
2024-07-04 18:10:36 +0200

    Add typescript language server
    The events package is needed since @open-rpc/client-js currently silently depends on it.

--- src/editor/typescript-editor.ts
+++ src/editor/typescript-editor.ts
@@ -1,9 +1,33 @@
 import { javascript } from "@codemirror/lang-javascript";
+import { StateEffect } from "@codemirror/state";
+import { WebSocketTransport } from "@open-rpc/client-js";
+import { languageServerWithTransport } from "codemirror-languageserver";
 import { FileEditor } from "./file-editor";
 
+const languageServerTransport = new WebSocketTransport("ws://localhost:9999");
+
 export class TypeScriptEditor extends FileEditor {
+  constructor(path: string) {
+    super(path);
+    // languageServer depends on this.path,
+    // which is not yet set during this.getExtensions
+    this.view.dispatch({
+      effects: StateEffect.appendConfig.of(this.languageServer()),
+    });
+  }
+
   *getExtensions() {
     yield* super.getExtensions();
     yield javascript({ typescript: true });
+  }
+
+  languageServer() {
+    return languageServerWithTransport({
+      languageId: "typescript",
+      rootUri: "source://.",
+      documentUri: `source://${this.path}`,
+      transport: languageServerTransport,
+      workspaceFolders: [],
+    });
   }
 }
