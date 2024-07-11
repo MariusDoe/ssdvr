f8ab0dcbcfb80efbab4e11ddf11d5295509cfa90
Marius Dörbandt <github.marius@doerbandt.de>
2024-07-11 20:01:06 +0200

    Add floor and ambient light

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import "core-js/actual";
+import {
+  AmbientLight,
+  Color,
+  Mesh,
+  MeshPhysicalMaterial,
+  PlaneGeometry,
+  TextureLoader,
+} from "three";
+import uvTexture from "../textures/uv_grid_opengl.jpg?url";
+import { Button } from "./button";
 import "./controllers";
 import "./demo";
 import "./editor/editor";
@@ -9,9 +19,33 @@ import { preserve } from "./hmr/preserve";
 import { materialFromColor } from "./materials";
 import { openInMovable, openInMovableScroller } from "./open";
 import "./scene";
+import { scene } from "./scene";
 import { TreeScrollerController } from "./tree";
 import "./vr";
 import { Workspace } from "./workspace";
+
+preserve("environment", async () => {
+  const texture = await new TextureLoader().loadAsync(uvTexture);
+  const floor = new Mesh(
+    new PlaneGeometry(5, 5),
+    new MeshPhysicalMaterial({ map: texture })
+  );
+
+  floor.rotateX(-Math.PI / 2);
+  scene.add(floor);
+  const light = new AmbientLight(new Color("white"));
+  let lightOn = true;
+  const updateLight = () => {
+    light.intensity = lightOn ? 0.8 : 0.1;
+  };
+  updateLight();
+  const lightSwitch = new Button(new Color("yellow"), () => {
+    lightOn = !lightOn;
+    updateLight();
+  });
+  scene.add(light, lightSwitch);
+  lightSwitch.position.set(1, 1, -1);
+});
 
 preserve("file-picker", () => {
   const filePicker = new FilePicker(".", {
